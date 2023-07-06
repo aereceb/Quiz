5fa42ae28a01a7748c11bd20f045509e18e52955
alisayali <alisayerecebali@gmail.com>
2023-07-06 09:51:12 +0300

    Code is ready

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,139 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try (BufferedWriter wr = new BufferedWriter(new FileWriter("questionsGeography.csv"))) {
-            //geography
-            wr.write("Which country is famous for the pyramids?,Egypt,Bulgaria,France,Russia");
-            wr.newLine();
-            wr.write("The capital of England is?,London,Targovishte,Berlin,Rome");
-            wr.newLine();
-            wr.write("On which island do lemurs live?,Madagascar,Cyprus,Greenland,Великобритания");
-            wr.newLine();
-            wr.write("In which country is the city of Seattle located?,USA,Canada,Mexico,UK");
-            wr.newLine();
-            wr.write("The highest peak in the world is?,Everest,Kangchenjunga,Lhotse,Manaslu");
-            wr.newLine();
-            wr.write("What is the capital of Bulgaria?,Sofia,Varna,Burgas,Targovishte");
-            wr.newLine();
-            wr.write("What do pandas eat?,Bamboo,Chicken,Snacks,Chocolate");
-            wr.newLine();
-            wr.write("What is the capital of Chile?,Santiago,Varna,Brazil,Mexico");
+    public static ArrayList<String[]> readQuestionsFromCSV(String filename) throws IOException {
+        ArrayList<String[]> questions = new ArrayList<>();
+        FileReader filereader = new FileReader(filename);
+        BufferedReader reader = new BufferedReader(filereader);
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] questionData = line.split(",");
+                questions.add(questionData);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }try (BufferedWriter wr = new BufferedWriter(new FileWriter("questionsScience.csv"))) {
-            wr.newLine();//science
-            wr.write("Where are neurons located?,Brain,Legs,Nowhere,Arms");
-            wr.newLine();
-            wr.write("What color is pure sulfur?,Yellow,White,Red,Black");
-            wr.newLine();
-            wr.write("What color are endangered species marked?,Red,Black,Gold,Rose");
-            wr.newLine();
-            wr.write("What is the name of the number 3.1415...?,Pi,Pokemon,One,Three");
-            wr.newLine();
-            wr.write("What can circle the earth 7.5 times in one second?,Light,Plane,Human,Rocket");
-            wr.newLine();
-            wr.write("Which animal or bird lays the largest eggs in the world?,Ostrich,Parrot,Crow,Flamingo");
-            wr.newLine();
-            wr.write("How do you spell the chemical element Astat?,At,As,Ta,Ast");
-            wr.newLine();
-            wr.write("What is the outside of the cell called?,Membrane,Core,Cytoplasm,DNA");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } finally {
+            reader.close();
         }
-             try (BufferedWriter wr = new BufferedWriter(new FileWriter("questionsSports.csv"))) {
-            wr.write("Who is Neymar?,football player,basketball player,tennis player,programmer");
-            wr.newLine();
-            wr.write("What is the most popular sport in the world?,Football,Basketball,Tennis,Swimming");
-            wr.newLine();
-            wr.write("What sport is played with a racket?,Tennis,Football,Volleyball,Boxing");
-            wr.newLine();
-            wr.write("Where did Aikido come from?,Japan,Chine,Bulgaria,Turkey");
-            wr.newLine();
-            wr.write("The most popular sport in Brazil is?,Football,Baseball,Volleyball,Weightlifting");
-            wr.newLine();
-            wr.write("What is ice hockey played with?,Stick,Ball,Nothing,Ice");
-            wr.newLine();
-            wr.write("Which athletes should be weighed before competition?,Boxers,Football player,Swimmers,Dancers");
-            wr.newLine();
-            wr.write("In which country is FC Roma located?,Italy,France,UK,Germany");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        return questions;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Quiz Game! Good luck!");
+        System.out.println("1. Geography");
+        System.out.println("2. Science");
+        System.out.println("3. Sports");
+        System.out.print("Choose a category:");
+        int category = input.nextInt();
+        if (category > 0 || category < 4) {
+            switch (category) {
+                case 1:
+                    int score = 0;
+                    ArrayList<String[]> questionsGeo = readQuestionsFromCSV("questionsGeography.csv");
+                    ArrayList<String[]> shuffledQuestionsGeo = new ArrayList<>(questionsGeo);
+                    Collections.shuffle(shuffledQuestionsGeo);
+                    for (int i = 0; i < shuffledQuestionsGeo.size(); i++) {
+                        String[] question = shuffledQuestionsGeo.get(i);
+                        System.out.println("Question " + (i + 1) + ":");
+                        String questionText = question[0];
+                        System.out.println(questionText);
+                        ArrayList<String> answers = new ArrayList<>();
+                        for (int j = 1; j < question.length; j++) {
+                            answers.add(question[j]);
+                        }
+                        Collections.shuffle(answers);
+                        for (int j = 0; j < answers.size(); j++) {
+                            String answer = answers.get(j);
+                            System.out.println((j + 1) + ". " + answer);
+                        }
+                        System.out.print("Answer:");
+                        int selectedAnswerIndex = input.nextInt();
+                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
+                        String correctAnswer = question[1];
+                        if (selectedAnswer.equals(correctAnswer)) {
+                            System.out.println("Correct!");
+                            score++;
+                        } else {
+                            System.out.println("Wrong answer. Game over!");
+                            break;
+                        }
+                    }
+                    System.out.println("Quiz game over. Your score: " + score + ". Congrats!!!");
+                    break;
+                case 2:
+                    int score1 = 0;
+                    ArrayList<String[]> questionsScience = readQuestionsFromCSV("questionsScience.csv");
+                    ArrayList<String[]> shuffledQuestionsScience = new ArrayList<>(questionsScience);
+                    Collections.shuffle(shuffledQuestionsScience);
+                    for (int i = 0; i < shuffledQuestionsScience.size(); i++) {
+                        String[] question = shuffledQuestionsScience.get(i);
+                        System.out.println("Question " + (i + 1) + ":");
+                        String questionText = question[0];
+                        System.out.println(questionText);
+                        ArrayList<String> answers = new ArrayList<>();
+                        for (int j = 1; j < question.length; j++) {
+                            answers.add(question[j]);
+                        }
+                        Collections.shuffle(answers);
+                        for (int j = 0; j < answers.size(); j++) {
+                            String answer = answers.get(j);
+                            System.out.println((j + 1) + ". " + answer);
+                        }
+                        System.out.print("Enter answer:");
+                        int selectedAnswerIndex = input.nextInt();
+                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
+                        String correctAnswer = question[1];
+                        if (selectedAnswer.equals(correctAnswer)) {
+                            System.out.println("Correct!");
+                            score1++;
+                        } else {
+                            System.out.println("Wrong answer. Game over!");
+                            break;
+                        }
+                    }
+                    System.out.println("Quiz game over. Your score: " + score1 + ". Congrats!!!");
+                    break;
+                case 3:
+                    int score2 = 0;
+                    ArrayList<String[]> questionsSports = readQuestionsFromCSV("questionsSports.csv");
+                    ArrayList<String[]> shuffledQuestions = new ArrayList<>(questionsSports);
+                    Collections.shuffle(shuffledQuestions);
+                    for (int i = 0; i < shuffledQuestions.size(); i++) {
+                        String[] question = shuffledQuestions.get(i);
+                        System.out.println("Question " + (i + 1) + ":");
+                        String questionText = question[0];
+                        System.out.println(questionText);
+                        ArrayList<String> answers = new ArrayList<>();
+                        for (int j = 1; j < question.length; j++) {
+                            answers.add(question[j]);
+                        }
+                        Collections.shuffle(answers);
+                        for (int j = 0; j < answers.size(); j++) {
+                            String answer = answers.get(j);
+                            System.out.println((j + 1) + ". " + answer);
+                        }
+                        System.out.print("Answer:");
+                        int selectedAnswerIndex = input.nextInt();
+                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
+                        String correctAnswer = question[1];
+                        if (selectedAnswer.equals(correctAnswer)) {
+                            System.out.println("Correct!");
+                            score2++;
+                        } else {
+                            System.out.println("Wrong answer. Game over!");
+                            break;
+                        }
+                    }
+                    System.out.println("Quiz game over. Your score: " + score2 + ". Congrats!!!");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
         }
     }
 }
