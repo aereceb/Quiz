97267e3ca4c0975235305afb17294b1db53170ca
alisayali <alisayerecebali@gmail.com>
2023-07-13 20:30:56 +0300

    Simplifying with method

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,52 @@ public class Main {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        } finally {
             reader.close();
+        }
         return questions;
+    }
+
+    public static void playQuizCategory(String categoryName, String csvFilename) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Let's start " + categoryName + " Quiz!");
+        int score = 0;
+        ArrayList<String[]> questions = readQuestionsFromCSV(csvFilename);
+        ArrayList<String[]> shuffledQuestions = new ArrayList<>(questions);
+        Collections.shuffle(shuffledQuestions);
+        for (int i = 0; i < shuffledQuestions.size(); i++) {
+            String[] question = shuffledQuestions.get(i);
+            System.out.print("Question " + (i + 1) + ": ");
+            String questionText = question[0];
+            System.out.println(questionText);
+            ArrayList<String> answers = new ArrayList<>();
+            for (int j = 1; j < question.length; j++) {
+                answers.add(question[j]);
+            }
+            Collections.shuffle(answers);
+            for (int j = 0; j < answers.size(); j++) {
+                String answer = answers.get(j);
+                System.out.println((j + 1) + ". " + answer);
+            }
+            System.out.print("Answer:");
+            int selectedAnswerIndex = input.nextInt();
+            if (selectedAnswerIndex < 5 && selectedAnswerIndex > 0) {
+                String selectedAnswer = answers.get(selectedAnswerIndex - 1);
+                String correctAnswer = question[1];
+                if (selectedAnswer.equals(correctAnswer)) {
+                    System.out.println();
+                    System.out.println("Correct!");
+                    System.out.println();
+                    score++;
+                } else {
+                    System.out.println("Wrong answer. Game over!");
+                    break;
+                }
+            } else {
+                System.out.println("Invalid answer!");
+            }
+        }
+        System.out.println("Quiz game ended. Your score is " + score + "." + "Congrats!");
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,120 +72,19 @@ public class Main {
         System.out.println("3. Sports");
         System.out.print("Choose a category:");
         int category = input.nextInt();
-        if (category > 0 || category < 4) {
-            switch (category) {
-                case 1:
-                    System.out.println("Let's start Geography Quiz!");
-                    int score1 = 0;
-                    ArrayList<String[]> questionsGeo = readQuestionsFromCSV("questionsGeography.csv");
-                    ArrayList<String[]> shuffledQuestionsGeo = new ArrayList<>(questionsGeo);
-                    Collections.shuffle(shuffledQuestionsGeo);
-                    for (int i = 0; i < shuffledQuestionsGeo.size(); i++) {
-                        String[] question = shuffledQuestionsGeo.get(i);
-                        System.out.print("Question " + (i + 1) + ": ");
-                        String questionText = question[0];
-                        System.out.println(questionText);
-                        ArrayList<String> answers = new ArrayList<>();
-                        for (int j = 1; j < question.length; j++) {
-                            answers.add(question[j]);
-                        }
-                        Collections.shuffle(answers);
-                        for (int j = 0; j < answers.size(); j++) {
-                            String answer = answers.get(j);
-                            System.out.println((j + 1) + ". " + answer);
-                        }
-                        System.out.print("Answer:");
-                        int selectedAnswerIndex = input.nextInt();
-                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
-                        String correctAnswer = question[1];
-                        if (selectedAnswer.equals(correctAnswer)) {
-                            System.out.println();
-                            System.out.println("Correct!");
-                            System.out.println();
-                            score1++;
-                        } else {
-                            System.out.println("Wrong answer. Game over!");
-                            break;
-                        }
-                    }
-                    System.out.println("Quiz game over. Your score: " + score1 + ". ");
-                    break;
-                case 2:
-                    System.out.println("Let's start Science Quiz!");
-                    int score2 = 0;
-                    ArrayList<String[]> questionsScience = readQuestionsFromCSV("questionsScience.csv");
-                    ArrayList<String[]> shuffledQuestionsScience = new ArrayList<>(questionsScience);
-                    Collections.shuffle(shuffledQuestionsScience);
-                    for (int i = 0; i < shuffledQuestionsScience.size(); i++) {
-                        String[] question = shuffledQuestionsScience.get(i);
-                        System.out.print("Question " + (i + 1) + ": ");
-                        String questionText = question[0];
-                        System.out.println(questionText);
-                        ArrayList<String> answers = new ArrayList<>();
-                        for (int j = 1; j < question.length; j++) {
-                            answers.add(question[j]);
-                        }
-                        Collections.shuffle(answers);
-                        for (int j = 0; j < answers.size(); j++) {
-                            String answer = answers.get(j);
-                            System.out.println((j + 1) + ". " + answer);
-                        }
-                        System.out.print("Enter answer:");
-                        int selectedAnswerIndex = input.nextInt();
-                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
-                        String correctAnswer = question[1];
-                        if (selectedAnswer.equals(correctAnswer)) {
-                            System.out.println();
-                            System.out.println("Correct!");
-                            System.out.println();
-                            score2++;
-                        } else {
-                            System.out.println("Wrong answer. Game over!");
-                            break;
-                        }
-                    }
-                    System.out.println("Quiz game over. Your score: " + score2 + ".");
-                    break;
-                case 3:
-                    System.out.println("Let's start Sport Quiz!");
-                    int score3 = 0;
-                    ArrayList<String[]> questionsSports = readQuestionsFromCSV("questionsSports.csv");
-                    ArrayList<String[]> shuffledQuestions = new ArrayList<>(questionsSports);
-                    Collections.shuffle(shuffledQuestions);
-                    for (int i = 0; i < shuffledQuestions.size(); i++) {
-                        String[] question = shuffledQuestions.get(i);
-                        System.out.print("Question " + (i + 1) + ": ");
-                        String questionText = question[0];
-                        System.out.println(questionText);
-                        ArrayList<String> answers = new ArrayList<>();
-                        for (int j = 1; j < question.length; j++) {
-                            answers.add(question[j]);
-                        }
-                        Collections.shuffle(answers);
-                        for (int j = 0; j < answers.size(); j++) {
-                            String answer = answers.get(j);
-                            System.out.println((j + 1) + ". " + answer);
-                        }
-                        System.out.print("Answer:");
-                        int selectedAnswerIndex = input.nextInt();
-                        String selectedAnswer = answers.get(selectedAnswerIndex - 1);
-                        String correctAnswer = question[1];
-                        if (selectedAnswer.equals(correctAnswer)) {
-                            System.out.println();
-                            System.out.println("Correct!");
-                            System.out.println();
-                            score3++;
-                        } else {
-                            System.out.println("Wrong answer. Game over!");
-                            break;
-                        }
-                    }
-                    System.out.println("Quiz game over. Your score: " + score3 + ". ");
-                    break;
-                default:
-                    System.out.println("Invalid choice!");
-                    break;
-            }
+        switch (category) {
+            case 1:
+                playQuizCategory("Geography", "questionsGeography.csv");
+                break;
+            case 2:
+                playQuizCategory("Science", "questionsScience.csv");
+                break;
+            case 3:
+                playQuizCategory("Sports", "questionsSports.csv");
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                break;
         }
     }
 }
